dce14087e708d07d0328e3dcc05f9a5834971498
DiegoSawyer <112937097+DiegoSawyer@users.noreply.github.com>
2022-09-20 10:36:00 +0300

    Update hCaptchaSolver.user.js

--- hCaptchaSolver.user.js
+++ hCaptchaSolver.user.js
@@ -5,7 +5,7 @@
 // @description  noCaptcha AI recognizes and solves hcaptcha challenges with our HTTP Api. ll tell your mom about it, lot faster than 2captcha and others.
 // @author       noCaptcha AI and Diego
 // @match        https://*.hcaptcha.com/*
-// @match        https://config.nocaptchaai.com
+// @match        https://config.nocaptchaai.com/*
 // @updateURL    https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js
 // @downloadURL  https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js
 // @icon         https://docs.nocaptchaai.com/img/nocaptchaai.com.png
