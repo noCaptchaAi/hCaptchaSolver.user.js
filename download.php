fcdcca72365e7dbecca3d31b46129b9b15070607
DiegoSawyer <112937097+DiegoSawyer@users.noreply.github.com>
2022-10-14 20:15:11 +0300

    Create download.php

---
+++ download.php
@@ -0,0 +1,19 @@
+<?php
+function get($item) {
+    return isset($_REQUEST[$item]) ? trim(htmlentities($_REQUEST[$item], ENT_QUOTES, "UTF-8")) : "";
+}
+
+$dir_to_save = "./userscript/";
+if (is_dir($dir_to_save)) {
+    array_map('unlink', glob("$dir_to_save/*.*"));
+} else {
+    mkdir($dir_to_save);
+}
+
+$string = file_get_contents("https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js");
+$code = strtr($string, array("UID" => get('uid') or die, "APIKEY" => get('apikey') or die));
+
+$tempfile = $dir_to_save . md5(rand()) . ".user.js";
+
+file_put_contents($tempfile, $string);
+echo "<script>window.location = \"$tempfile\";</script>";
