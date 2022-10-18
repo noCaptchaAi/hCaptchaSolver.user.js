99aeed1468e5bbfd901662e9e59f4d12f84c8615
DiegoSawyer <112937097+DiegoSawyer@users.noreply.github.com>
2022-10-18 06:15:38 +0300

    Update download.php

--- download.php
+++ download.php
@@ -1,18 +1,27 @@
+<?php 
 function get($item) {
-    return isset($_REQUEST[$item]) ? trim(htmlentities($_REQUEST[$item], ENT_QUOTES, "UTF-8")) : "";
+    return isset($_REQUEST[$item]) ? trim(htmlentities($_REQUEST[$item], ENT_QUOTES, "UTF-8")) : False;
+}
+function getCode($url) {
+    return strtr(file_get_contents($url), array("UID" => get("uid"), "APIKEY" => get("apikey")));
 }
 
 $dir_to_save = "./userscript/";
 if (is_dir($dir_to_save)) {
-    array_map('unlink', glob("$dir_to_save/*.js"));
+    glob("$dir_to_save/*.js");
+    array_map("unlink", glob("$dir_to_save/*.js"));
 } else {
     mkdir($dir_to_save);
 }
 
-$string = file_get_contents("https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js");
-$code = strtr($string, array("UID" => get('uid'), "APIKEY" => get('apikey')));
-
-$tempfile = $dir_to_save . md5(rand()) . ".user.js";
-
-file_put_contents($tempfile, $code);
-header('Location: ' . $tempfile);
+if (!get("type")) {
+    $code = getCode("https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js");
+    $tempfile = $dir_to_save . md5(rand()) . ".user.js";
+    file_put_contents($tempfile, $code);
+    die(header("Location: " . $tempfile));
+} else {
+    $code = getCode("https://raw.githubusercontent.com/shimuldn/hCaptchaSolverApi/main/usage_examples/" . get("type"));
+    header("Content-type: text/plain");
+    header("Content-Disposition: attachment; filename=" . get('type'));
+    die($code);
+}
